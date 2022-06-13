e870f0e71d680f43adcd79e002ae359e7884cf58
Manuel <martmanu@sheridancollege.ca>
2022-06-13 11:26:08 -0400

    Comment, Manuel Martinez

---
+++ Project1_SKAM/src/project1_skam/Project1_SKAM.java
@@ -0,0 +1,22 @@
+
+
+package project1_skam;
+
+/**
+ *
+ * @author Manuel Martinez
+ */
+public class Project1_SKAM 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+
+}
+
+//Comment was successful
