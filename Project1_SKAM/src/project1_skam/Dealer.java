9be9ac83cd7aa5d0993199e0c8940980ce503eda
Manuel <martmanu@sheridancollege.ca>
2022-06-13 12:36:09 -0400

    Changes 12:35pm Monday, Manuel Martinez

---
+++ Project1_SKAM/src/project1_skam/Dealer.java
@@ -0,0 +1,6 @@
+package project1_skam;
+
+public class Dealer {
+
+	public String test;
+}
---
+++ Project1_SKAM/src/project1_skam/Player.java
@@ -0,0 +1,36 @@
+package project1_skam;
+import java.util.ArrayList;
+
+public class Player {
+
+	public String name;
+	public int points;
+	public int numWins;
+	public ArrayList hand;
+
+	public Player() {
+		// TODO - implement Player.Player
+		throw new UnsupportedOperationException();
+	}
+
+	public void hitOrStand() {
+		// TODO - implement Player.hitOrStand
+		throw new UnsupportedOperationException();
+	}
+
+	public void addPoints() {
+		// TODO - implement Player.addPoints
+		throw new UnsupportedOperationException();
+	}
+
+	public void addWins() {
+		// TODO - implement Player.addWins
+		throw new UnsupportedOperationException();
+	}
+
+	public void addCards() {
+		// TODO - implement Player.addCards
+		throw new UnsupportedOperationException();
+	}
+
+}
