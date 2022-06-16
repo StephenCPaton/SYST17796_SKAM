891cc2363b273a77ed064c2ab6ea174c1a3a831a
Manny13M <105446720+Manny13M@users.noreply.github.com>
2022-06-16 16:30:15 -0400

    Update Player.java

--- Project1_SKAM/src/project1_skam/Player.java
+++ Project1_SKAM/src/project1_skam/Player.java
@@ -9,28 +9,23 @@ public class Player {
 	public ArrayList hand;
 
 	public Player() {
-		// TODO - implement Player.Player
-		throw new UnsupportedOperationException();
+
 	}
 
 	public void hitOrStand() {
-		// TODO - implement Player.hitOrStand
-		throw new UnsupportedOperationException();
+
 	}
 
 	public void addPoints() {
-		// TODO - implement Player.addPoints
-		throw new UnsupportedOperationException();
+
 	}
 
 	public void addWins() {
-		// TODO - implement Player.addWins
-		throw new UnsupportedOperationException();
+
 	}
 
 	public void addCards() {
-		// TODO - implement Player.addCards
-		throw new UnsupportedOperationException();
+
 	}
 
 }
