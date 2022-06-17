a29d9cab09ac2bfaa616b223b49216632d88ee0b
Manuel <martmanu@sheridancollege.ca>
2022-06-17 16:35:55 -0400

    Added Enums to Card and constructor and methods to dealer, Manuel Martinez

--- Project1_SKAM/src/project1_skam/Card.java
+++ Project1_SKAM/src/project1_skam/Card.java
@@ -7,14 +7,69 @@ package project1_skam;
 // @author manuelmartinez
 public class Card 
 {
-    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    public String suit;
-    public int value; //1-13
-    
-    
-    public Card(String suit, int value)
+    public enum Suit
     {
-        this.suit = suit;
-        this.value = value;
+        HEARTS("Hearts"), 
+        DIAMONDS("Diamonds"), 
+        SPADES("Spades"), 
+        CLUBS("Clubs");
+
+        String name;
+
+        Suit(String name)
+        {
+            this.name = name;
+        }
+    }
+    
+    
+    public enum Value
+    {
+        ACE ("Ace", 1), 
+        TWO ("Two", 2), 
+        THREE ("Three", 3), 
+        FOUR ("Four", 4), 
+        FIVE ("Five", 5), 
+        SIX ("Six", 6), 
+        SEVEN ("Seven", 7), 
+        EIGHT ("Eight", 8), 
+        NINE ("Nine", 9), 
+        TEN ("Ten", 10), 
+        JACK ("Jack", 10), 
+        QUEEN ("Queen", 10), 
+        KING ("King", 10);
+        
+        String name;
+        int value;
+        
+        Value(String name, int value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+    
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Colour colour,Rank rank)
+    {
+        this.colour=colour;
+        this.rank=rank;
+    }
+    /**
+     * @return the suit
+     */
+    public Colour getColour() 
+    {
+        return colour;
+    }
+
+    /**
+     * @return the value
+     */
+    public Rank getRank() 
+    {
+        return rank;
     }
 }
--- Project1_SKAM/src/project1_skam/Dealer.java
+++ Project1_SKAM/src/project1_skam/Dealer.java
@@ -3,5 +3,15 @@ package project1_skam;
 public class Dealer extends Player
 {
 
+	public Dealer() {
+		// TODO - implement Dealer.Dealer
+		throw new UnsupportedOperationException();
+	}
+
+	public void dealerMove() {
+		// TODO - implement Dealer.dealerMove
+		throw new UnsupportedOperationException();
+	}
+
 	
 }
